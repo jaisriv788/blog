6bc69289796aaae539738a7ed6a6862c2d102260
ByteFlow <byteflow007@gmail.com>
2024-02-25 23:12:11 +0530

    minor changes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,11 +4,6 @@ import blog from "../routes/blog";
 
 const app = new Hono().basePath("/api/v1");
 
-//Home Route --------------------------------------------------------------------
-app.get("/", (c) => {
-  return c.json({ messgae: "This is bloggig website home page for backend." });
-});
-
 //all user related route
 app.route("/", user);
 
